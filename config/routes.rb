f42dd4519a8d4c8490704ae9ecf901375b78abad
Ashley Boehs <ashley@boehs.com>
2023-10-02 16:34:06 -0500

    Update permissions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #
   devise_for :users
   resources :passwords do
-    resources :shares
+    resources :shares, only: [:new, :create, :destroy]
   end
 
   # Defines the root path route ("/")
